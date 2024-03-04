f1dd47fe6714c95789daf91898049411b007229c
Mihael Berčič <mihael@regnum.si>
2024-03-04 01:38:31 +0100

    Added message signing and verificaiton.

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,9 +1,14 @@
-use hmac::{Hmac, Mac};
 use hmac::digest::MacError;
-use p256::elliptic_curve::rand_core;
-use p256::SecretKey;
-use rand_core::OsRng;
+use hmac::{Hmac, Mac};
+use p256::ecdsa::signature::Signer;
+use p256::ecdsa::{Signature, SigningKey};
+use p256::elliptic_curve::rand_core::OsRng;
 use sha2::{Digest, Sha256};
+
+type HmacSha256 = Hmac<Sha256>;
+
+const CRYPTO_GROUP_SIZE_BITS: usize = 256;
+const CRYPTO_GROUP_SIZE_BYTES: usize = 32;
 
 /// Uses SHA-256 to hash the provided message.
 pub fn hash_message(message: &[u8]) -> [u8; 32] {
@@ -12,30 +17,36 @@ pub fn hash_message(message: &[u8]) -> [u8; 32] {
     let result = hasher.finalize();
     let mut output_data: [u8; 32] = [0; 32];
     output_data.copy_from_slice(&result[..]);
-    return output_data;
+    output_data
 }
 
-type HmacSha256 = Hmac<Sha256>;
-
 /// Generates a hash of the [message] with the provided [key];
-pub fn hmac(key: &[u8], message: &[u8]) -> [u8; 32] {
+pub fn hmac(key: &[u8], message: &[u8]) -> [u8; CRYPTO_GROUP_SIZE_BYTES] {
     let mut mac = HmacSha256::new_from_slice(key).expect("HMAC should take any key size");
     mac.update(message);
     let result = mac.finalize();
-    let mut output = [0u8; 32];
+    let mut output = [0u8; CRYPTO_GROUP_SIZE_BYTES];
     output.copy_from_slice(&result.into_bytes()[..]);
-    return output;
+    output
 }
 
 /// Verifies HMAC hash using the [key], [message] and [hashed message](code_bytes);
 pub fn verify_hmac(key: &[u8], message: &[u8], code_bytes: &[u8]) -> Result<(), MacError> {
     let mut mac = HmacSha256::new_from_slice(key).expect("HMAC should take any key size");
     mac.update(message);
-    return mac.verify_slice(&code_bytes[..]);
+    mac.verify_slice(&code_bytes[..])
 }
 
-/// Generates a Public-Private key pair using NIST-P256
-pub fn generate_key_pair() -> SecretKey {
-    let secret = p256::SecretKey::random(&mut OsRng);
-    return secret;
+/// Generates a Public - Private key pair using NIST-P256
+pub fn generate_key_pair() -> SigningKey {
+    SigningKey::random(&mut OsRng)
+}
+
+/// Signs a message using the [key] into signature which is of length [2 * CRYPTO_GROUP_SIZE_BYTES]
+/// TODO: Check in case of BigEndian errors
+pub fn sign_message(key: &SigningKey, message: &[u8]) -> [u8; 2 * CRYPTO_GROUP_SIZE_BYTES] {
+    let signed: Signature = key.sign(message);
+    let mut signature_bytes = [0u8; 2 * CRYPTO_GROUP_SIZE_BYTES];
+    signature_bytes.copy_from_slice(&signed.to_bytes()[..]);
+    signature_bytes
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,12 @@ mod crypto;
 
 #[cfg(test)]
 mod tests {
+    use p256::{
+        ecdsa::{signature::Verifier, Signature, VerifyingKey},
+        pkcs8::DecodePublicKey,
+        EncodedPoint,
+    };
+
     use super::*;
 
     #[test]
@@ -9,7 +15,10 @@ mod tests {
         let sample = b"mihael";
         let hash = crypto::hash_message(sample);
         let hex = hash.map(|x| format!("{:02x}", x)).join("");
-        assert_eq!(hex, "a1ec7aff7a3ce85b3784176861b4995fe092eea0f417443d4ba77ae96a9f812e");
+        assert_eq!(
+            hex,
+            "a1ec7aff7a3ce85b3784176861b4995fe092eea0f417443d4ba77ae96a9f812e"
+        );
     }
 
     #[test]
@@ -18,14 +27,18 @@ mod tests {
         let sample_input_message = b"input message";
         let hmac = crypto::hmac(sample_key, sample_input_message);
         let hex = hex::encode(hmac);
-        assert_eq!(hex, "97d2a569059bbcd8ead4444ff99071f4c01d005bcefe0d3567e1be628e5fdcd9")
+        assert_eq!(
+            hex,
+            "97d2a569059bbcd8ead4444ff99071f4c01d005bcefe0d3567e1be628e5fdcd9"
+        )
     }
 
     #[test]
     fn crypto_hmac_verify() {
         let sample_key = b"my secret and secure key";
         let sample_input_message = b"input message";
-        let x = hex::decode("97d2a569059bbcd8ead4444ff99071f4c01d005bcefe0d3567e1be628e5fdcd9").unwrap();
+        let x = hex::decode("97d2a569059bbcd8ead4444ff99071f4c01d005bcefe0d3567e1be628e5fdcd9")
+            .unwrap();
         let hmac = crypto::verify_hmac(sample_key, sample_input_message, &x);
         hmac.expect("Verification was not successful.")
     }
@@ -33,5 +46,23 @@ mod tests {
     #[test]
     fn generate_key_pair() {
         let _key_pair = crypto::generate_key_pair();
+    }
+
+    #[test]
+    fn sign_message() {
+        let signing_key = crypto::generate_key_pair();
+        let message = b"Test";
+        let signed = crypto::sign_message(&signing_key, message);
+    }
+
+    #[test]
+    fn verify_signed_message() {
+        let public_key_encoded = hex::decode("044f85bb78121be98ce0644cb9ae2e97d86d24bf962acabecdfa26e15f425fa0dbcafb3b8c65f5bc1f14af6e176b46bf20a58058f69d2f6d05ce91f4c44e16c5f8").unwrap();
+        let message = b"Test";
+        let public_key = EncodedPoint::from_bytes(&public_key_encoded).unwrap();
+        let verifying_key = VerifyingKey::from_encoded_point(&public_key).unwrap();
+        let signature = hex::decode("7d639959c1a701326cb6827f10b59dca871d4f5f7d80f1c898eb6d85ac37999376afc3b4e22bd7724730cf1648f8dc974d1c5df8f94380f43fbe6414b3677a77").unwrap();
+        let decoded_signature = Signature::from_slice(&signature).unwrap();
+        verifying_key.verify(message, &decoded_signature).is_ok();
     }
 }
