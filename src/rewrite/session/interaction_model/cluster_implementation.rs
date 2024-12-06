aa56f6af50786c593fca0aaca7c8349c6bba8fca
Mihael Berčič <mihael@regnum.si>
2024-12-06 13:36:05 +0100

    Added some documentation comments.

--- src/rewrite/session/interaction_model/cluster_implementation.rs
+++ src/rewrite/session/interaction_model/cluster_implementation.rs
@@ -6,6 +6,13 @@ use crate::{
 };
 
 pub trait ClusterImplementation: Any {
+    /// Read attributes of a cluster based on the [AttributePath] provided.
+    ///
+    /// Returns a vector of attribute reports for each specific attribute read.
     fn read_attribute(&self, path: &AttributePath) -> Vec<AttributeReport>;
+
+    /// Used for Box<dyn ClusterImplementation> storage and abstraction of clusters.
+    ///
+    /// Needed for device endpoint storage and easier manueverability.
     fn as_any(&mut self) -> &mut dyn Any;
 }
